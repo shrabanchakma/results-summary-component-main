bde78fb8c57eb18a28c6ee075cb1f94517683f2c
Shraban Chakma <shrabanchakma328@gmail.com>
2024-02-06 00:34:00 +0600

    added stats

---
+++ index.js
@@ -0,0 +1,8 @@
+// const stats = document.querySelector(".stats");
+// const newDiv = document.createElement("div");
+// const categoryDiv = document.createElement("div");
+// const scoreDiv = document.createElement("div");
+// newDiv.textContent = "something";
+// newDiv.appendChild(categoryDiv);
+// newDiv.appendChild(scoreDiv);
+// stats.appendChild(newDiv);
