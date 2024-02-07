637c6eeec82f7ae01dec1161d216d120199ba358
Shraban Chakma <shrabanchakma328@gmail.com>
2024-02-07 23:27:04 +0600

    made responsive for mobile and skills are added dynamically

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
-// const stats = document.querySelector(".stats");
-// const newDiv = document.createElement("div");
-// const categoryDiv = document.createElement("div");
-// const scoreDiv = document.createElement("div");
-// newDiv.textContent = "something";
-// newDiv.appendChild(categoryDiv);
-// newDiv.appendChild(scoreDiv);
-// stats.appendChild(newDiv);
+const skillList = document.querySelector(".skillList");
+console.log(skillList);
+async function addSkills() {
+  try {
+    const res = await fetch("data.json");
+    const skills = await res.json();
+    console.log(skills);
+
+    skills.map((skill) => {
+      // creating skillLeft element
+      const skillLeft = document.createElement("div");
+      skillLeft.classList.add("skillLeft");
+
+      const skillImg = document.createElement("div");
+      skillImg.classList.add("skillImg");
+      const image = document.createElement("img");
+      image.src = skill.icon;
+      image.alt = "skill icon";
+      skillImg.appendChild(image);
+
+      const skillName = document.createElement("p");
+      skillName.classList.add("skillName");
+      skillName.textContent = skill.category;
+
+      //   appending skill image and skill name in skillLeft
+      skillLeft.appendChild(skillImg);
+      skillLeft.appendChild(skillName);
+
+      // creating skillScore element
+      const skillScore = document.createElement("p");
+      skillScore.classList.add("skillScore");
+      const score = document.createElement("span");
+      score.classList.add("score");
+      score.textContent = skill.score;
+      score.insertAdjacentHTML("beforeend", "/100");
+
+      //   appending skillLeft and skillScore to the skill(list) element;
+      const skillElement = document.createElement("li");
+      skillElement.classList.add("skill");
+      skillElement.appendChild(skillLeft);
+      skillElement.appendChild(skillScore);
+
+      //   appending it to the parent skillList element
+      skillList.appendChild(skillElement);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+addSkills();
